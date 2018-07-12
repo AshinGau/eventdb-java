183b8568dbd067f095d60c0e399a685b6ec3d265
gaoxin.helloworld <gaoxin.helloworld@bytedance.com>
2018-07-12 18:48:54 +0800

    statistic restful api

--- src/main/java/org/osv/eventdb/fits/FitsEventDBClient.java
+++ src/main/java/org/osv/eventdb/fits/FitsEventDBClient.java
@@ -1,19 +1,15 @@
 package org.osv.eventdb.fits;
 
 import java.io.IOException;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.Cell;
-import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
@@ -21,10 +17,8 @@ import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.filter.KeyOnlyFilter;
 import org.apache.hadoop.hbase.filter.PrefixFilter;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.osv.eventdb.event.PropertyValue;
+import org.osv.eventdb.hbase.Command;
 import org.osv.eventdb.util.ConfigProperties;
-import org.osv.eventdb.util.Pair;
-import org.xerial.snappy.Snappy;
 
 public class FitsEventDBClient {
 	private ConfigProperties configProp;
@@ -53,14 +47,110 @@ public class FitsEventDBClient {
 		eventMetaTable = conn.getTable(TableName.valueOf(configProp.getProperty("fits.meta.table")));
 	}
 
-	public List<String> list() throws IOException{
+	public String[] list() throws IOException {
 		Scan scan = new Scan();
 		PrefixFilter tablepre = new PrefixFilter(Bytes.toBytes("table#"));
 		KeyOnlyFilter keyonly = new KeyOnlyFilter();
 		scan.setFilter(tablepre).setFilter(keyonly);
-		ResultScanner results =  eventMetaTable.getScanner(scan);
-		return null;
+		ResultScanner results = eventMetaTable.getScanner(scan);
+		List<String> tables = new ArrayList<String>();
+		for (Result row : results)
+			tables.add(Bytes.toString(row.getRow()));
+		String[] arr = new String[tables.size()];
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = tables.get(i);
+		return arr;
 	}
 
+	public long getTotalEvents() throws IOException {
+		return Bytes.toLong(eventMetaTable.get(new Get(Bytes.toBytes("total"))).getValue(Command.dataBytes,
+				Bytes.toBytes("events")));
+	}
 
+	public long getTotalFiles() throws IOException {
+		return Bytes.toLong(eventMetaTable.get(new Get(Bytes.toBytes("total"))).getValue(Command.dataBytes,
+				Bytes.toBytes("files")));
+	}
+
+	public long getEventsOfTable(String tableName) throws IOException {
+		return Bytes.toLong(eventMetaTable.get(new Get(Bytes.toBytes("table#" + tableName))).getValue(Command.dataBytes,
+				Bytes.toBytes("events")));
+	}
+
+	public long getFilesOfTable(String tableName) throws IOException {
+		return Bytes.toLong(eventMetaTable.get(new Get(Bytes.toBytes("table#" + tableName))).getValue(Command.dataBytes,
+				Bytes.toBytes("files")));
+	}
+
+	public String[] getFileListOfTable(String tableName) throws IOException {
+		Scan scan = new Scan();
+		PrefixFilter tablepre = new PrefixFilter(Bytes.toBytes(tableName + "#"));
+		KeyOnlyFilter keyonly = new KeyOnlyFilter();
+		scan.setFilter(tablepre).setFilter(keyonly);
+		ResultScanner results = eventMetaTable.getScanner(scan);
+		List<String> files = new ArrayList<String>();
+		for (Result row : results) {
+			String key = Bytes.toString(row.getRow());
+			files.add(key.substring(tableName.length() + 1));
+		}
+		String[] arr = new String[files.size()];
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = files.get(i);
+		return arr;
+	}
+
+	public int[] getWriteSpeed(long startTimeStamp, long endTimeStamp) throws IOException {
+		Scan scan = new Scan(Bytes.toBytes("et#" + startTimeStamp), Bytes.toBytes("et#" + endTimeStamp));
+		ResultScanner results = eventMetaTable.getScanner(scan);
+		int start = (int) (startTimeStamp / 1000);
+		int end = (int) (endTimeStamp / 1000);
+		int num = end - start + 1;
+		int[] speed = new int[num];
+
+		long eventsPre = 0;
+		for (Result row : results) {
+			long time = new Long(Bytes.toString(row.getRow()).substring(3));
+			long events = Bytes.toLong(row.getValue(Command.dataBytes, Bytes.toBytes("events")));
+			int tip = (int) ((time - startTimeStamp) / 1000);
+			if (eventsPre != 0 && tip < num)
+				speed[tip] += (int) (events - eventsPre);
+			eventsPre = events;
+		}
+		return speed;
+	}
+
+	public int[] getWriteSpeed(int lastSeconds) throws IOException {
+		long now = System.currentTimeMillis();
+		return getWriteSpeed(now - lastSeconds * 1000, now);
+	}
+
+	public int[] getReadLatency(long startTimeStamp, long endTimeStamp) throws IOException {
+		Scan scan = new Scan(Bytes.toBytes("d#" + startTimeStamp), Bytes.toBytes("et#" + endTimeStamp));
+		ResultScanner results = eventMetaTable.getScanner(scan);
+		int start = (int) (startTimeStamp / 1000);
+		int end = (int) (endTimeStamp / 1000);
+		int num = end - start + 1;
+		int[] delay = new int[num];
+		int[] cnt = new int[num];
+
+		for (Result row : results) {
+			long time = new Long(Bytes.toString(row.getRow()).substring(2));
+			long dl = Bytes.toLong(row.getValue(Command.dataBytes, Bytes.toBytes("delay")));
+			int tip = (int) ((time - startTimeStamp) / 1000);
+			if (tip < num) {
+				cnt[tip] += 1;
+				delay[tip] += dl;
+			}
+		}
+		for (int i = 0; i < num; i++) {
+			delay[i] /= cnt[i];
+		}
+
+		return delay;
+	}
+
+	public int[] getReadLatency(int lastSeconds) throws IOException {
+		long now = System.currentTimeMillis();
+		return getReadLatency(now - lastSeconds * 1000, now);
+	}
 }
--- src/main/java/org/osv/eventdb/fits/util/RestServer.java
+++ src/main/java/org/osv/eventdb/fits/util/RestServer.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.osv.eventdb.fits.FitsEventDBClient;
 import org.osv.eventdb.fits.HeQueryClient;
 
 import fi.iki.elonen.NanoHTTPD;
@@ -22,6 +23,7 @@ public class RestServer extends NanoHTTPD {
 
   public static void runAtPort(int port) {
     try {
+      client = new FitsEventDBClient();
       new RestServer(port);
     } catch (IOException ioe) {
       System.err.println("Couldn't start server:\n" + ioe);
@@ -32,14 +34,92 @@ public class RestServer extends NanoHTTPD {
   public static Pattern STATIC_SRC = Pattern.compile("/static/(.+)");
   public static Map<String, String> MIME_TYPE = NanoHTTPD.mimeTypes();
   public static String EVENTDB_ENV = System.getenv("EVENTDB_ENV");
+  public static String EVENTDB_HOME = System.getenv("EVENTDB_HOME");
+  public static FitsEventDBClient client;
 
   public static InputStream getResourceAsStream(String path) throws IOException {
     if ("debug".equals(EVENTDB_ENV)) {
-      String staticRoot = "src/main/resources/";
+      String staticRoot = EVENTDB_HOME + "/src/main/resources/";
       return new FileInputStream(staticRoot + path);
     } else {
       return RestServer.class.getClassLoader().getResourceAsStream(path);
     }
+  }
+
+  public static Response clientGetTableList() throws IOException {
+    StringBuffer sb = new StringBuffer();
+    String[] tables = client.list();
+    if (tables.length > 0)
+      sb.append(tables[0]);
+    for (int i = 1; i < tables.length; i++)
+      sb.append("," + tables[i]);
+    return newFixedLengthResponse(Status.OK, NanoHTTPD.MIME_PLAINTEXT, sb.toString());
+  }
+
+  public static Response clientGetTotalEvents() throws IOException {
+    return newFixedLengthResponse(Status.OK, NanoHTTPD.MIME_PLAINTEXT, String.valueOf(client.getTotalEvents()));
+  }
+
+  public static Response clientGetTotalFiles() throws IOException {
+    return newFixedLengthResponse(Status.OK, NanoHTTPD.MIME_PLAINTEXT, String.valueOf(client.getTotalFiles()));
+  }
+
+  public static Response clientGetEventsOfTable(String table) throws IOException {
+    return newFixedLengthResponse(Status.OK, NanoHTTPD.MIME_PLAINTEXT, String.valueOf(client.getEventsOfTable(table)));
+  }
+
+  public static Response clientGetFilesOfTable(String table) throws IOException {
+    return newFixedLengthResponse(Status.OK, NanoHTTPD.MIME_PLAINTEXT, String.valueOf(client.getFilesOfTable(table)));
+  }
+
+  public static Response clientGetFileListOfTable(String table) throws IOException {
+    StringBuffer sb = new StringBuffer();
+    String[] files = client.getFileListOfTable(table);
+    if (files.length > 0)
+      sb.append(files[0]);
+    for (int i = 1; i < files.length; i++)
+      sb.append("," + files[i]);
+    return newFixedLengthResponse(Status.OK, NanoHTTPD.MIME_PLAINTEXT, sb.toString());
+  }
+
+  public static Response clientGetWriteSpeed(String start, String end) throws IOException {
+    StringBuffer sb = new StringBuffer();
+    int[] speed = client.getWriteSpeed(Long.valueOf(start), Long.valueOf(end));
+    if (speed.length > 0)
+      sb.append(speed[0]);
+    for (int i = 1; i < speed.length; i++)
+      sb.append("," + speed[i]);
+    return newFixedLengthResponse(Status.OK, NanoHTTPD.MIME_PLAINTEXT, sb.toString());
+  }
+
+  public static Response clientGetWriteSpeedOfLast(String seconds) throws IOException {
+    StringBuffer sb = new StringBuffer();
+    int[] speed = client.getWriteSpeed(Integer.valueOf(seconds));
+    if (speed.length > 0)
+      sb.append(speed[0]);
+    for (int i = 1; i < speed.length; i++)
+      sb.append("," + speed[i]);
+    return newFixedLengthResponse(Status.OK, NanoHTTPD.MIME_PLAINTEXT, sb.toString());
+  }
+
+  public static Response clientGetReadLatency(String start, String end) throws IOException {
+    StringBuffer sb = new StringBuffer();
+    int[] speed = client.getReadLatency(Long.valueOf(start), Long.valueOf(end));
+    if (speed.length > 0)
+      sb.append(speed[0]);
+    for (int i = 1; i < speed.length; i++)
+      sb.append("," + speed[i]);
+    return newFixedLengthResponse(Status.OK, NanoHTTPD.MIME_PLAINTEXT, sb.toString());
+  }
+
+  public static Response clientGetReadLatencyOfLast(String seconds) throws IOException {
+    StringBuffer sb = new StringBuffer();
+    int[] speed = client.getReadLatency(Integer.valueOf(seconds));
+    if (speed.length > 0)
+      sb.append(speed[0]);
+    for (int i = 1; i < speed.length; i++)
+      sb.append("," + speed[i]);
+    return newFixedLengthResponse(Status.OK, NanoHTTPD.MIME_PLAINTEXT, sb.toString());
   }
 
   @Override
@@ -48,9 +128,35 @@ public class RestServer extends NanoHTTPD {
       String uri = session.getUri();
       Matcher heQuery = HE_QUERY.matcher(uri);
       Matcher staticSrc = STATIC_SRC.matcher(uri);
+
       if (uri.equals("") || uri.equals("/") || uri.equals("/index") || uri.equals("/index.html")) {
         return newChunkedResponse(Status.OK, NanoHTTPD.MIME_HTML, getResourceAsStream("html/index.html"));
       } else if (heQuery.matches()) {
+        if (heQuery.group(1).equals("eventdb:info")) {
+          Map<String, String> params = session.getParms();
+          String op = params.get("op");
+          if ("tableList".equals(op))
+            return clientGetTableList();
+          else if ("totalEvents".equals(op))
+            return clientGetTotalEvents();
+          else if ("totalFiles".equals(op))
+            return clientGetTotalFiles();
+          else if ("eventsOfTable".equals(op))
+            return clientGetEventsOfTable(params.get("table"));
+          else if ("filesOfTable".equals(op))
+            return clientGetFilesOfTable(params.get("table"));
+          else if ("fileListOfTable".equals(op))
+            return clientGetFileListOfTable(params.get("table"));
+          else if ("writeSpeed".equals(op))
+            return clientGetWriteSpeed(params.get("startTimeStamp"), params.get("endTimeStamp"));
+          else if ("writeSpeedOfLast".equals(op))
+            return clientGetWriteSpeedOfLast(params.get("lastSeconds"));
+          else if ("readLatency".equals(op))
+            return clientGetReadLatency(params.get("startTimeStamp"), params.get("endTimeStamp"));
+          else if ("readLatencyOfLast".equals(op))
+            return clientGetReadLatencyOfLast(params.get("lastSeconds"));
+        }
+
         HeQueryClient client = new HeQueryClient(heQuery.group(1));
         List<byte[]> result = client.query(session.getQueryParameterString());
         List<HeEventDecoder.He> evts = HeEventDecoder.decode(result);
@@ -58,7 +164,8 @@ public class RestServer extends NanoHTTPD {
         sb.append("time,detID,channel,pulse,eventType");
         for (HeEventDecoder.He he : evts)
           sb.append(String.format("\n%f,%d,%d,%d,%d", he.time, he.detID & 0x00ff, he.channel & 0x00ff,
-                  he.pulse & 0x00ff, he.eventType & 0x00ff));
+              he.pulse & 0x00ff, he.eventType & 0x00ff));
+        client.close();
         return newFixedLengthResponse(Status.OK, NanoHTTPD.MIME_PLAINTEXT, sb.toString());
       } else if (staticSrc.matches()) {
         String file = staticSrc.group(1);
